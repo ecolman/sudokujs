60c4330b2f620fad06feefe4ab55e05e8d440e5c
Eric Colman <ecolman@gmail.com>
2017-09-14 11:01:18 -0400

    Added constants file

---
+++ src/js/constants.js
@@ -0,0 +1,48 @@
+﻿'use strict';
+
+// sudoku board enums
+export const sudokuUniqueResult = {
+  unique: 'unique',
+  notUnique: 'not unique',
+  noSolution: 'no solution'
+};
+
+// menu enums
+export const gameView = {
+  menu: 'menu',
+  board: 'board',
+  options: 'options'
+};
+
+export const menuOptionType = {
+  easy: 'easy',
+  medium: 'medium',
+  hard: 'hard',
+  expert: 'expert',
+  load: 'load',
+  resume: 'resume',
+  options: 'options'
+};
+
+export const optionType = {
+  timer: 'timer',
+  highlight: 'highlight',
+  feedback: 'feedback',
+  penalize: 'penalize',
+  notesMode: 'notesMode',
+  autoRemoveNotes: 'autoRemoveNotes',
+  reverseSelector: 'reverseSelector'
+};
+
+export const boardLoadType = {
+  fresh: 'fresh',
+  resume: 'resume',
+  load: 'load'
+};
+
+export const boardDifficulty = {
+  easy: 'Easy',
+  medium: 'Medium',
+  hard: 'Hard',
+  expert: 'Expert'
+};
