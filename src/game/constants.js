9a33eb010cbed7064bfc18eb4433aec466e8f702
Eric Colman <ecolman@gmail.com>
2020-09-16 19:01:12 -0400

    Removed custom generator and solver

--- src/game/constants.js
+++ src/game/constants.js
@@ -3,12 +3,3 @@ export const ROWS = [0, 1, 2, 3, 4, 5, 6, 7, 8];
 export const BLANK_ROW = new Array(0, 0, 0, 0, 0, 0, 0, 0, 0);
 
 export const BOARDS_TO_ATTEMPT = 1000;
-export const ATTEMPTS_PER_BOARD = 10;
-export const UNIQUENESS_ATTEMPTS = 1;
-export const WEB_WORKERS_ENABLED = true;
-
-export const UNIQUE_RESULT = {
-  UNIQUE: 'unique',
-  NOT_UNIQUE: 'not_unique',
-  NO_SOLUTION: 'no_solution',
-}
